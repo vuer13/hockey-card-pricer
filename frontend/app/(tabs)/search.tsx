b349f65d5c184684a59316dad4bf7543c5abb381
Ryan Vu <ryanvu1131@gmail.com>
2025-12-31 01:21:58 -0800

    moved search to search page

--- frontend/app/(tabs)/search.tsx
+++ frontend/app/(tabs)/search.tsx
@@ -1,12 +1,25 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
+import { Image, ScrollView, View } from "react-native";
+import SearchBar from "@/components/SearchBar";
+import { useRouter } from "expo-router";
 
-const search = () => {
-  return (
-    <View>
-      <Text>search</Text>
-    </View>
-  )
+export default function Index() {
+    const router = useRouter();
+
+    return (
+        <View className="flex-1 bg-primary">
+            <Image source={images.bg} className="absolute w-full z-0" />
+
+            <ScrollView className="flex-1 px-5" showsVerticalScrollIndicator={false} contentContainerStyle={{ minHeight: '100%', paddingBottom: 10 }}>
+                <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
+                <View className="flex-1 mt-5">
+                    <SearchBar 
+                        onPress={() => router.push('/search')} 
+                        placeholder="Search for cards"
+                    />
+                </View>
+            </ScrollView>
+        </View>
+    );
 }
-
-export default search
