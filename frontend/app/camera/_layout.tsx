5ac70c1efe55a407303dcf88bbe07e51939702f4
Ryan Vu <ryanvu1131@gmail.com>
2026-01-02 01:52:18 -0800

    camera setup

---
+++ frontend/app/camera/_layout.tsx
@@ -0,0 +1,19 @@
+import { Stack } from "expo-router";
+import React from 'react'
+
+const _layout = () => {
+    return (
+        <Stack>
+            <Stack.Screen
+                name="capture"
+                options={{ headerShown: false, }}
+            />
+            <Stack.Screen
+                name="info"
+                options={{ headerShown: false, }}
+            />
+        </Stack>
+    )
+}
+
+export default _layout
