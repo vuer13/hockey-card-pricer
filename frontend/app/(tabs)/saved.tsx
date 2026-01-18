c9ac7a4124bf8df45d36417c4c699d0065f43456
Ryan Vu <ryanvu1131@gmail.com>
2026-01-17 23:17:46 -0800

    stubs

--- frontend/app/(tabs)/saved.tsx
+++ frontend/app/(tabs)/saved.tsx
@@ -1,12 +1,36 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useRouter } from 'expo-router';
 
-const saved = () => {
-  return (
-    <View>
-      <Text>saved</Text>
-    </View>
-  )
+const API_BASE = process.env.EXPO_PUBLIC_API_BASE_HOME;
+
+interface Card {
+    id: string;
+    name: string;
+    card_series: string;
+    card_number: string;
+    team_name: string;
+    image: string;
 }
 
-export default saved
+const Saved = () => {
+    const router = useRouter();
+    const [cards, setCards] = useState<Card[]>([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        fetchSavedCards();
+    }, []);
+
+    const fetchSavedCards = async () => {
+        // TODO
+    };
+
+    return (
+        <View>
+            <Text>saved</Text>
+        </View>
+    )
+}
+
+export default Saved
