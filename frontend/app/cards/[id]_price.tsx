c4b3b3de52991c95deabb588e89788781d42e382
Ryan Vu <ryanvu1131@gmail.com>
2026-01-17 18:31:36 -0800

    stubs

--- frontend/app/cards/[id]_price.tsx
+++ frontend/app/cards/[id]_price.tsx
@@ -1,12 +1,29 @@
-import { useLocalSearchParams } from 'expo-router';
-import React from 'react'
+import { useLocalSearchParams, useRouter } from 'expo-router';
+import { useEffect, useState } from 'react'
 import { View } from 'react-native'
 
 const PriceDetails = () => {
+    const router = useRouter();
     const { id } = useLocalSearchParams();
+
+    const [loading, setLoading] = useState(false);
+    const [history, setHistory] = useState([]);
+    const [chart, setChart] = useState(null);
+
+    useEffect(() => {
+        if (id) {
+            // TODO - fetch history
+        }
+    }, [id]);
+
+    const fetchHistory = async () => {
+        // TODO - API call to get history
+    };
     
     return (
-        <View></View>
+        <View>
+            {/* TODO - Render price history and chart */}
+        </View>
     )
 }
 
