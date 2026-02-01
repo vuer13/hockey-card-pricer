58f57ccf31de8821c0eda6523ba1973845e4d74e
Ryan Vu <ryanvu1131@gmail.com>
2026-02-01 02:41:41 -0800

    components before style

--- frontend/app/(auth)/login.tsx
+++ frontend/app/(auth)/login.tsx
@@ -1,7 +1,19 @@
+import { signInWithEmail } from '@/auth/supabase';
 import React from 'react'
 import { View } from 'react-native';
 
 export default function Login() {
+    const [email, setEmail] = React.useState('');
+    const [password, setPassword] = React.useState('');
+    const [error, setError] = React.useState<string | null>(null);
+
+    const handleLogin = async () => {
+        const {error} = await signInWithEmail(email, password);
+        if (error) {
+            setError(error.message);
+        }
+    };
+    
     return (
         <View>
             {/* Login Screen Content */}
