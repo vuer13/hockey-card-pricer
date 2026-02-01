9bbf183957d767e60a3890c28539b8fea95676cb
Ryan Vu <ryanvu1131@gmail.com>
2026-02-01 02:39:56 -0800

    stub for login

---
+++ frontend/app/(auth)/login.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View } from 'react-native';
+
+export default function Login() {
+    return (
+        <View>
+            {/* Login Screen Content */}
+        </View>
+    );
+}
