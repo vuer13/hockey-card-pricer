4bac16e9443db6d80702b40b778c04db8c70af27
Ryan Vu <ryanvu1131@gmail.com>
2026-02-02 00:49:40 -0800

    signout button in profile page

--- frontend/app/(tabs)/profile.tsx
+++ frontend/app/(tabs)/profile.tsx
@@ -1,12 +1,14 @@
-import { View, Text } from 'react-native'
+import { View, Text, Button } from 'react-native'
 import React from 'react'
+import { signOut } from '@/auth/supabase'
 
 const Profile = () => {
-  return (
-    <View>
-      <Text>Profile</Text>
-    </View>
-  )
+    return (
+        <View>
+            <Text>Profile</Text>
+            <Button title="Logout" onPress={signOut} />
+        </View>
+    )
 }
 
 export default Profile
