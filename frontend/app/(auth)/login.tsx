6b8c10d449373267d8d8744d90f8188718758869
Ryan Vu <ryanvu1131@gmail.com>
2026-02-02 00:42:10 -0800

    login page

--- frontend/app/(auth)/login.tsx
+++ frontend/app/(auth)/login.tsx
@@ -1,22 +1,30 @@
 import { signInWithEmail } from '@/auth/supabase';
+import { useRouter } from 'expo-router';
 import React from 'react'
-import { View } from 'react-native';
+import { Button, TextInput, View } from 'react-native';
+import { Text } from 'react-native-svg';
 
 export default function Login() {
+    const router = useRouter();
+    
     const [email, setEmail] = React.useState('');
     const [password, setPassword] = React.useState('');
     const [error, setError] = React.useState<string | null>(null);
 
     const handleLogin = async () => {
-        const {error} = await signInWithEmail(email, password);
+        const { error } = await signInWithEmail(email, password);
         if (error) {
             setError(error.message);
         }
     };
-    
+
     return (
         <View>
-            {/* Login Screen Content */}
+            <TextInput placeholder="Email" onChangeText={setEmail} />
+            <TextInput placeholder="Password" secureTextEntry onChangeText={setPassword} />
+            {error && <Text>{error}</Text>}
+            <Button title="Login" onPress={handleLogin} />
+            <Button title="Sign up" onPress={() => router.push("/signup")} />
         </View>
     );
 }
