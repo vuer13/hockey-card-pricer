d7631098741914d63d4ae30e74066cb5c1bbd151
Ryan Vu <ryanvu1131@gmail.com>
2026-02-06 13:55:49 -0800

    stub

---
+++ frontend/lib/api.ts
@@ -0,0 +1,7 @@
+import { supabase } from "@/auth/supabase";
+
+const API_URL = process.env.EXPO_PUBLIC_API_BASE_HOME
+
+export async function apiFetch () {
+    // TODO: Implement API fetch logic here with auth
+};
