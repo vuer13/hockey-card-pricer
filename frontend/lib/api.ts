d394f03be3af175935dcf35fb96ff35351fc40ec
Ryan Vu <ryanvu1131@gmail.com>
2026-02-06 13:57:54 -0800

    apiFetch to add auth

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -2,6 +2,17 @@ import { supabase } from "@/auth/supabase";
 
 const API_URL = process.env.EXPO_PUBLIC_API_BASE_HOME
 
-export async function apiFetch () {
-    // TODO: Implement API fetch logic here with auth
-};
+export async function apiFetch(path: string, options: RequestInit = {}) {
+    const { data: { session } } = await supabase.auth.getSession();
+    
+    const token = session?.access_token;
+
+    return fetch(`${API_URL}${path}`, {
+        ...options,
+        headers: {
+            ...(options.headers || {}),
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+        },
+    });
+};    
