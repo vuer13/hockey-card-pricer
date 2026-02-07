2dc909af9f722c939b13e262727a31435350b19f
Ryan Vu <ryanvu1131@gmail.com>
2026-02-07 00:15:52 -0800

    signout styling lol

--- frontend/app/(tabs)/profile.tsx
+++ frontend/app/(tabs)/profile.tsx
@@ -1,13 +1,23 @@
-import { View, Text, Button } from 'react-native'
+import { View, Text, Button, Pressable } from 'react-native'
 import React from 'react'
 import { signOut } from '@/auth/supabase'
 
 const Profile = () => {
     return (
-        <View>
-            <Text>Profile</Text>
-            <Button title="Logout" onPress={signOut} />
-        </View>
+        <View className="flex-1 justify-center items-center px-6" >
+            <View className="w-full max-w-md items-center">
+                <Text className="text-xl mb-6">
+                    Profile
+                </Text>
+
+                <Pressable
+                    onPress={signOut}
+                    className="border rounded py-2 px-6"
+                >
+                    <Text>Logout</Text>
+                </Pressable>
+            </View>
+        </View >
     )
 }
 
